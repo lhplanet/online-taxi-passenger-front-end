4b88409decb4fae513004c77eb68b65e5a848285
lihaopeng <752869332@qq.com>
2023-08-31 20:13:43 +0800

    完成整体流程

---
+++ src/config/gdMapConf.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: ch cwl_ch@163.com
+ * @Date: 2022-12-28 14:39:29
+ * @LastEditors: ch
+ * @LastEditTime: 2023-01-03 21:00:14
+ * @Description: 高德地图配置，需要自行去高德开放平台申请
+ */
+export default {
+    // 高德地图JS Api key
+    key:'60daece4e0ae0ced0aff48d8a7d45a65',
+    // 高德地图JS Api key对应的秘钥，正式环境最好不要放前端
+    securityJsCode : 'd61010d02f21371741865152a6db8291',
+    // 城市获取key
+    cityKey : '78462a297c925d75cbfbf4e58c8e1b95',
+    // 高德城市请求地址
+    cityApiUrl : 'https://restapi.amap.com/v3/config/district',
+    // 默认选中城市
+    city: {
+        adcode: "110000",
+        center: "116.407387,39.904179",
+        citycode: "010",
+        name: "北京市"
+    }
+}
---
+++ src/pages/login.vue
@@ -0,0 +1,167 @@
+<template>
+  <view class="container">
+    <view class="logo" @dblclick="handleAccount">网约车-乘客端</view>
+    <view class="login">
+      <input placeholder="请输入手机号" type="number" maxlength="11" v-model="phone" class="login--input"/>
+      <view class="login--code">
+        <input placeholder="请输入验证码" type="number" maxlength="6" class="login--input" v-model="code"/>
+        <text @click="handleGetVerifyCode" :class="['login--send-btn', {'login--send-btn__disabled':isDisableCode}]">
+          {{ codeText }}
+        </text>
+      </view>
+    </view>
+    <button class="login--btn" @click="handleLogin">登录</button>
+  </view>
+</template>
+
+
+<script setup>
+import {_IsPhone} from '@gykeji/jsutil';
+import {computed, onMounted, ref} from 'vue';
+import {ApiGetVerifyCode, ApiPostVerifyCodeCheck, ApiGetPassengerInfo} from '../api/user'
+import {HandleApiError, ShowToast} from '../utils';
+import {useStore} from 'vuex';
+
+const $store = useStore();
+const token = computed(() => $store.state.token);
+// 获取验证码按钮文字
+let codeText = ref('获取验证码');
+// 重新获取验证码倒计时
+let codeTimerNum = ref(0);
+// 重新获取验证码倒计时定时器
+let codeTimer = null;
+// 获取验证码按钮是否禁用
+let isDisableCode = computed(() => codeTimerNum.value !== 0);
+// 手机号输入框内容
+let phone = ref('');
+// 验证码输入框内容
+let code = ref('');
+
+// 如果已经登录，token还有效，直接跳转到首页
+onMounted(() => {
+  if (token.value) {
+    uni.redirectTo({url: '/pages/index/index'});
+  }
+})
+
+// 获取验证码
+const handleGetVerifyCode = async () => {
+  // 验证手机号
+  if (isDisableCode.value || !verifyPhone(phone.value)) {
+    return false;
+  }
+  // 设置重新获取验证码倒计时
+  codeTimerNum.value = 10;
+  // 开始倒计时
+  calcTimer();
+  // 发送验证码
+  const {error, result} = await ApiGetVerifyCode({passengerPhone: phone.value});
+  // 如果发送成功，显示提示
+  console.log(error, {passengerPhone: phone.value})
+  if (!HandleApiError(error)) {
+    ShowToast('验证码发送成功');
+  }
+}
+
+// 验证手机号
+const verifyPhone = (phone) => {
+  let result = phone && _IsPhone(phone);
+  if (!result) {
+    ShowToast('请填写正确手机号！');
+    result = false;
+  }
+  return result;
+}
+
+// 重新获取验证码倒计时
+const calcTimer = () => {
+  codeTimerNum.value--;
+  if (codeTimerNum.value === 0) {
+    clearTimeout(codeTimer);
+    codeText.value = '获取验证码';
+    return false;
+  }
+  codeText.value = `${codeTimerNum.value}s后重新获取`;
+  setTimeout(() => {
+    calcTimer();
+  }, 1000)
+}
+
+// 登录
+const handleLogin = async () => {
+  if (!verifyPhone(phone.value)) {
+    return false;
+  }
+  if (!code.value) {
+    ShowToast('请输入正确验证码');
+    return false;
+  }
+  // 校验验证码
+  const {error, result} = await ApiPostVerifyCodeCheck({
+    passengerPhone: phone.value,
+    verificationCode: code.value
+  });
+  // 如果校验成功，保存token，跳转到首页
+  if (!HandleApiError(error)) {
+    $store.commit('setToken', result.accessToken);
+    uni.redirectTo({url: '/pages/index/index'});
+    getPassengerInfo();
+  }
+}
+
+// 获取乘客信息
+const getPassengerInfo = async () => {
+  const {error, result} = await ApiGetPassengerInfo({passengerPhone: phone.value});
+
+  console.log("--------------");
+  console.log(result);
+
+  if (!HandleApiError(error)) {
+    $store.commit('setUserInfo', result);
+  }
+}
+
+// TODO: 双击logo跳转到账号管理页面，方便测试，上线时删除
+const handleAccount = () => {
+  uni.navigateTo({url: '/pages/account'})
+}
+</script>
+
+
+<style lang="scss" scoped>
+.logo{
+	font-size: 46rpx;
+	text-align: center;
+	margin: 100rpx 0 84rpx 0;
+	font-weight: bold;
+}
+.login{
+	width: 650rpx;
+	margin: 0 auto;
+	&--input{
+		border-bottom: 2rpx solid $uni-border-color;
+		width: 650rpx;
+		height: 100rpx;
+		font-size: $uni-font-size-lg;
+	}
+	&--code{
+		position: relative;
+	}
+	&--send-btn{
+		position: absolute;
+		right: 0;
+        top: 36rpx;
+		color: $uni-color-primary;
+		&__disabled{
+            color: $uni-text-color-disable;
+		}
+	}
+	&--btn{
+		width: 650rpx;
+		font-size: $uni-font-size-lg;
+		margin: 60rpx 50rpx 40rpx;
+        background: $uni-color-primary;
+        color: $uni-text-color-inverse;
+	}
+}
+</style>
